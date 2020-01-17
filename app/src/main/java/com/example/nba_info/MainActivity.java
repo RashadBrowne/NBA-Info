e5b6758277092175a38d9efe0984ec1c6cd9b426
ShadowCodeRz <shadowrebornrz@gmail.com>
2020-01-16 21:18:16 -0400

    Ver2.352 - Final portrait design and partial rotation support

--- app/src/main/java/com/example/nba_info/MainActivity.java
+++ app/src/main/java/com/example/nba_info/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.nba_info;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.transition.Slide;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -22,12 +24,15 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Send
             fragmentMain = new MainFragment();//Innit the fragment
             fragmentMain.setExitTransition(new Slide(Gravity.LEFT));
             fragmentMain.setEnterTransition(new Slide(Gravity.RIGHT));
-            ft.add(R.id.container, fragmentMain).commit();
-            //Swap it into the FrameLayout with the id container
-            Log.d("hey", "onCreate: 1");
-        }
-        else {
-            Log.d("hey", "onCreate: 2");
+
+            if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+                ft.add(R.id.container, fragmentMain).commit();
+                //Swap it into the FrameLayout with the id container
+            }
+
+            else if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                onInfoSent("","", "", "");
+            }
         }
     }
 
@@ -36,9 +41,15 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Send
         fragmentDetail= new DetailFragment(Url , Name, Desc, Link);//Innit the fragment
         fragmentDetail.setExitTransition(new Slide(Gravity.LEFT));
         fragmentDetail.setEnterTransition(new Slide(Gravity.RIGHT));
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.container, fragmentDetail).addToBackStack(null).commit();
+        }
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragmentDetail).addToBackStack(null).commit();
-        Log.d("hey", "onCreate: 3");
+        else if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.container2, fragmentDetail).addToBackStack(null).commit();
+            Log.d("hey", "onCreate: 3");
+        }
     }
 }
